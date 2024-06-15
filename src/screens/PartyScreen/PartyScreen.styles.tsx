b1aa251e761cfa278a165b315eb520f63b3b4acf
Viktor JOVANOVIC <viktoryova@outlook.com>
2024-06-15 10:45:22 +0200

    feat(party): setup initial page

---
+++ src/screens/PartyScreen/PartyScreen.styles.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const PartyScreenStyles = StyleSheet.create({
+	container: { flex: 1 },
+});
+
+export default PartyScreenStyles;
---
+++ src/screens/PartyScreen/PartyScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const PartyScreen = () => {
+	return <View>Party screen</View>;
+};
+
+export default PartyScreen;
