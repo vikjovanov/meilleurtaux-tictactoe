f45d4e7fbc763decc883b62153985670706c6354
Viktor JOVANOVIC <viktoryova@outlook.com>
2024-06-15 10:45:22 +0200

    feat(home): setup initial page

---
+++ src/screens/HomeScreen/HomeScreen.styles.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const HomeScreenStyles = StyleSheet.create({
+	container: { flex: 1 },
+});
+
+export default HomeScreenStyles;
---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const HomeScreen = () => {
+	return <View>Home screen</View>;
+};
+
+export default HomeScreen;
