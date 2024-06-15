11b6f0f9405c77feb7ea24a667dbe067ca1c92cc
Viktor JOVANOVIC <viktoryova@outlook.com>
2024-06-15 10:45:22 +0200

    feat(history): setup initial page

---
+++ src/screens/HistoryScreen/HistoryScreen.styles.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const HistoryScreenStyles = StyleSheet.create({
+	container: { flex: 1 },
+});
+
+export default HistoryScreenStyles;
---
+++ src/screens/HistoryScreen/HistoryScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const HistoryScreen = () => {
+	return <View>History screen</View>;
+};
+
+export default HistoryScreen;
